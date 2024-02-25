2675932671827ee2c18ee8d55ab5f86a9d550854
Rewa2230 <159625620+Rewa2230@users.noreply.github.com>
2024-02-25 12:39:23 +0300

    Add files via upload

--- SpeedTrigger.cs
+++ SpeedTrigger.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedTrigger : MonoBehaviour
+public class SpeedTrigger: MonoBehaviour
 {
     public float speedFactor = 2.5f;
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
-        
+        //Увелечение скорости игрока
+        other.GetComponent<FirstPersonMovement>().runSpeed *= speedFactor;
     }
 
     // Update is called once per frame
     void OnTriggerExit(Collider other)
     {
-        
+        //Понижение скорости игрока
+        other.GetComponent<FirstPersonMovement>().runSpeed /= speedFactor;
     }
 }
